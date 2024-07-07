88cc4eb1bd137a4114a150129eed449aeff58109
linareslinares <martineznose24@gmail.com>
2024-07-06 20:29:52 -0600

    update reportCommand.php

--- src/linareslinares/command/reportCommand.php
+++ src/linareslinares/command/reportCommand.php
@@ -6,7 +6,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use linareslinares\ReportUI;
-use pocketmine\utils\TextFotmat;
+use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 
 class reportCommand extends Command {
@@ -16,11 +16,18 @@ class reportCommand extends Command {
     $this->setPermission("report.command");
   }
   
-  public function execute(CommandSender $sender, string $commandLabel, array $args) {
+  public function execute(CommandSender $sender, string $commandLabel, array $args) : void {
     
     if ($sender instanceof Player){
       
+      if (!$sender->hasPermission("report.command")){
+        $sender->sendMessage(TextFormat::YELLOW. "No tienes permisos para usar este comando.");
+      }
+      
+      $sender->sendMessage(TextFormat::GREEN. "Menu de reportes");
+      
     }
+    
   }
   
 }
